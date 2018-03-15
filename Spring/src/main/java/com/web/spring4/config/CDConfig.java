0a7dff079b76a1bb03536563f3a18de066cf0b0d
Garden <847686279@qq.com>
2018-03-15 20:00:17 +0800

    添加运行时值注入Demo

--- Spring/src/main/java/com/web/spring4/config/CDConfig.java
+++ Spring/src/main/java/com/web/spring4/config/CDConfig.java
@@ -1,17 +1,43 @@
 package com.web.spring4.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 import com.web.spring4.bean.CompactDisc;
 import com.web.spring4.bean.impl.JayChouAlbum;
+import com.web.spring4.bean.impl.JayChouAlbum3;
 
 @Configuration
+@PropertySource("classpath:/data.properties")
 public class CDConfig {
+	
+	@Autowired
+	private Environment env;
 	
 	@Bean
 	public  CompactDisc jayChouAlbum(){
 		return new JayChouAlbum();
+	}
+	
+//	@Bean
+//	public CompactDisc jayChouAlbum3(){
+//		return new JayChouAlbum3(env.getProperty("album.songs"),env.getProperty("album.artist"));
+//	}
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer placeholderCongigurer(){
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+	
+	@Bean
+	public CompactDisc jayChouAlbum3(@Value("${album.songs}") String songs
+			,@Value("${album.artist}") String artist){
+		return new JayChouAlbum3(songs,artist);
 	}
 	
 }
